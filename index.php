eab88553ed357946796ead0c29817c5e18c86923
carlos18619 <30670010+carlos18619@users.noreply.github.com>
2017-09-25 11:28:08 -0300

    introduzindo novo erro

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ echo "<h1>Hello, Northeast PHP";
 echo "<h1><br>Hello, Northeast PHP";
 echo "<h1><br>Hello, OVO está aqui";
 echo "<h1><br>Hello, OVO retirou linha com erro";
+echo "introduzinho erro ...
 
