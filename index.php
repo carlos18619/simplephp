c17f8c0c71d42fd27f87942da5b546c2d684eb1b
carlos18619 <30670010+carlos18619@users.noreply.github.com>
2017-09-25 11:22:39 -0300

    retira linha com erro

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 echo "<h1>Hello, Northeast PHP";
 echo "<h1><br>Hello, Northeast PHP";
 echo "<h1><br>Hello, OVO está aqui";
-echo "e se tiver um erro aqui.....
+echo "<h1><br>Hello, OVO retirou linha com erro";
 
