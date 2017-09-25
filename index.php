351d0d017822a610e542ace1c8da1dc670deb860
carlos18619 <30670010+carlos18619@users.noreply.github.com>
2017-09-25 12:12:05 -0300

    retirando erro...

--- index.php
+++ index.php
@@ -4,5 +4,4 @@ echo "<h1>Hello, Northeast PHP";
 echo "<h1><br>Hello, Northeast PHP";
 echo "<h1><br>Hello, OVO está aqui";
 echo "<h1><br>Hello, OVO retirou linha com erro";
-echo "introduzinho erro ...
 
