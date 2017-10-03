1ec023dcbb8f6e70286f311739b1a63f53315056
carlos18619 <30670010+carlos18619@users.noreply.github.com>
2017-10-03 13:00:48 -0300

    voce aqui de novo

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 echo "<h1>Hello, Northeast PHP";
 echo "<h1><br>Hello, Northeast PHP";
 echo "<h1><br>Hello, OVO está aqui";
-echo "<h1><br>Hello, OVO retirou linha com erro";
+echo "<h1><br>Hello, OVO. Você aqui, de novo? ";
 
