817af2a9e057665d802ed1d0c84c403948f04829
carlos18619 <30670010+carlos18619@users.noreply.github.com>
2017-10-04 16:20:48 -0300

    DE NOVO DE NOVO

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ echo "<h1>Hello, Northeast PHP";
 echo "<h1><br>Hello, Northeast PHP";
 echo "<h1><br>Hello, OVO está aqui";
 echo "<h1><br>Hello, OVO. Você aqui, de novo? ";
+echo "<h1><br>Hello, OVO. Você aqui, de novo DE NOVO? ";
 
